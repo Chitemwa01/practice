035f7fe26649b063b1ef30d5d22af8f3376e6358
chitemwa01 <chitetembo@gmail.com>
2023-10-22 15:53:20 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+if __name__ == "__main__":
+
+    a =100
+    b = 67
+
+    if a < b:
+        print("true")
+
+    elif a==b:
+        print("false")
+        
+    else:
+        print("false")
+
+
+
+
+
+
+
